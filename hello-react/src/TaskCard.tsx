1184f8b04206200dc145a0a852567250a6a1501a
Libin Thankayathil <libinthankayathil@gmail.com>
2025-05-23 14:49:27 +0400

    minor change

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -3,8 +3,8 @@ import './TaskCard.css';
 interface TaskCardProps {
   title: string;
   assigneeName: string;
-  dueDate?: string;
-  completedAtDate?: string;
+  dueDate: string;
+  completedAtDate: string;
 }
 
 const TaskCard = (props: TaskCardProps): JSX.Element => {
@@ -13,11 +13,11 @@ const TaskCard = (props: TaskCardProps): JSX.Element => {
       <h2 className='text-2xl font-bold text-red-600'>{props.title}</h2>
       <p>
         {props.completedAtDate 
-          ? "Completed at: " + props.completedAtDate 
-          : "Due Date: " + props.dueDate
+          ? "Completed on:" + props.completedAtDate 
+          : "Due on:" + props.dueDate
         }
       </p>
-      <p>Assignee: {props.assigneeName}</p>
+      <p>Assignee:{props.assigneeName}</p>
     </div>
   );
 };
