df32d63996bc00ecd12743a1856794b826867514
Libin Thankayathil <libinthankayathil@gmail.com>
2025-05-28 21:35:45 +0400

    minor changes

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -14,9 +14,9 @@ const Task = (props: TaskProp) => {
     <div className="TaskItem shadow-md border border-slate-100">
         <div className="flex justify-between items-start">
           <div>
-            <Link to={`/tasks/${props.id}`} className="hover:text-blue-600 transition-colors">
+            <a href={`/tasks/${props.id || ""}`}>
               <h2 className="text-base font-bold my-1">{props.title}</h2>
-            </Link>
+            </a>
             <p className="text-sm text-slate-500 flex items-center gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
