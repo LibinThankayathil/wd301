03749d4128df010d9e55bc106f81a8062cd1d702
Libin Thankayathil <libinthankayathil@gmail.com>
2025-06-18 19:41:40 +0400

    added application level state

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,14 +1,17 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { RouterProvider } from "react-router-dom";
+import "./App.css";
 import router from "./routes"
 import { ThemeContext } from "./context/theme";
+import { ProjectsProvider } from "./context/projects/context";
 
 const App = () => {
   const { theme } = useContext(ThemeContext)
   return (
-    <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
-      {theme}
-      <RouterProvider router={router} />
+     <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
+      <ProjectsProvider>
+        <RouterProvider router={router} />
+      </ProjectsProvider>
     </div>
   );
 }
